fadc2b642317d88680a0a9f1664f270c02b533ab
Hekmat <hb8info@gmail.com>
2023-09-18 00:45:12 +0330

    Complete babel and webpack related modifications.

--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -18,32 +18,34 @@ class Assets {
 		$this->setup_hooks();
 	}
 
-	protected function setup_hooks(): void {
-		// actions and filters
-		add_action('wp_enqueue_scripts', [ $this, 'register_styles']);
-		add_action('wp_enqueue_scripts', [ $this, 'register_scripts']);
+	protected function setup_hooks() {
 
+		/**
+		 * Actions.
+		 */
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_styles' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_scripts' ] );
 	}
-	public function register_styles(): void {
 
-		// Register Styles.
+	public function register_styles() {
+		// Register styles.
+		wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime( AQUILA_DIR_PATH . '/style.css' ), 'all' );
+		wp_register_style( 'bootstrap-css', AQUILA_DIR_URI . '/assets/src/library/css/bootstrap.min.css', [], false, 'all' );
 
-		wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
-		wp_register_style('bootstrap-css', get_template_directory_uri() .'/assets/src/library/css/bootstrap.min.css', [], false, 'all');
-
-		// Enqueue Styles
-		wp_enqueue_style('style-css');
-		wp_enqueue_style('bootstrap-css');
-
+		// Enqueue Styles.
+		wp_enqueue_style( 'style-css' );
+		wp_enqueue_style( 'bootstrap-css' );
 	}
-	public function register_scripts(): void {
-		// Register Scripts.
-		wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', ['jquery'], filemtime(get_template_directory() . '/assets/main.js'), true);
-		wp_register_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.bundle.min.js', ['jquery'], false, true);
 
+	public function register_scripts() {
+		// Register scripts.
+		wp_register_script( 'main-js', AQUILA_BUILD_JS_URI . '/main.js', ['jquery'], filemtime( AQUILA_BUILD_JS_DIR_PATH . '/main.js' ), true );
+		wp_register_script( 'bootstrap-js', AQUILA_DIR_URI . '/assets/src/library/js/bootstrap.min.js', [ 'jquery' ], false, true );
 
 		// Enqueue Scripts.
-		wp_enqueue_script('main-js');
-		wp_enqueue_script('bootstrap-js');
+		wp_enqueue_script( 'main-js' );
+		wp_enqueue_script( 'bootstrap-js' );
 	}
+
+
 }
