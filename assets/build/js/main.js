1d87322eb94c8212f1e004129fca71d613468ad6
Hekmat <hb8info@gmail.com>
2023-09-20 01:20:45 +0330

    Add scss and fonts

---
+++ assets/build/js/main.js
@@ -0,0 +1,167 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/clock/index.js":
+/*!*******************************!*\
+  !*** ./src/js/clock/index.js ***!
+  \*******************************/
+/***/ (function() {
+
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+console.warn('Hello People!!!');
+// main.js
+/****Clock*****/
+(function ($) {
+  var Clock = /*#__PURE__*/function () {
+    function Clock() {
+      _classCallCheck(this, Clock);
+      this.initializeClock();
+    }
+    _createClass(Clock, [{
+      key: "initializeClock",
+      value: function initializeClock() {
+        var _this = this;
+        var t = setInterval(function () {
+          return _this.time();
+        }, 1000);
+      }
+    }, {
+      key: "numPad",
+      value: function numPad(str) {
+        var cStr = str.toString();
+        if (cStr.length < 2) str = 0 + cStr;
+        return str;
+      }
+    }, {
+      key: "time",
+      value: function time() {
+        var currDate = new Date();
+        var currSec = currDate.getSeconds();
+        var currMin = currDate.getMinutes();
+        var curr24Hr = currDate.getHours();
+        var ampm = curr24Hr >= 12 ? 'pm' : 'am';
+        var currHr = curr24Hr % 12;
+        currHr = currHr ? currHr : 12;
+        var stringTime = currHr + ':' + this.numPad(currMin) + ':' + this.numPad(currSec);
+        var timeEmojiEl = $('#time-emoji');
+        if (curr24Hr >= 5 && curr24Hr <= 17) {
+          timeEmojiEl.text('🌞');
+        } else {
+          timeEmojiEl.text('🌜');
+        }
+        $('#time').text(stringTime);
+        $('#ampm').text(ampm);
+      }
+    }]);
+    return Clock;
+  }();
+  new Clock();
+})(jQuery);
+
+/***/ }),
+
+/***/ "./src/sass/main.scss":
+/*!****************************!*\
+  !*** ./src/sass/main.scss ***!
+  \****************************/
+/***/ (function() {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleParseError: Module parse failed: Unexpected character '\u0000' (1:4)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n(Source code omitted for this binary file)\n    at handleParseError (C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\webpack\\lib\\NormalModule.js:982:19)\n    at C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\webpack\\lib\\NormalModule.js:1101:5\n    at processResult (C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\webpack\\lib\\NormalModule.js:806:11)\n    at C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\webpack\\lib\\NormalModule.js:866:5\n    at C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\loader-runner\\lib\\LoaderRunner.js:407:3\n    at iterateNormalLoaders (C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:10)\n    at C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\loader-runner\\lib\\LoaderRunner.js:224:4\n    at C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\webpack\\lib\\NormalModule.js:840:15\n    at Array.eval (eval at create (C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\webpack\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:12:1)\n    at runCallbacks (C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\webpack\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:43:15)\n    at C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\webpack\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:249:5\n    at C:\\xampp\\htdocs\\aquila\\wp-content\\themes\\aquila\\assets\\node_modules\\graceful-fs\\graceful-fs.js:123:16\n    at FSReqCallback.readFileAfterClose [as oncomplete] (node:internal/fs/read_file_context:68:3)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+!function() {
+"use strict";
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../sass/main.scss */ "./src/sass/main.scss");
+/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_sass_main_scss__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _clock__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./clock */ "./src/js/clock/index.js");
+/* harmony import */ var _clock__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_clock__WEBPACK_IMPORTED_MODULE_1__);
+console.warn('Hello People!!!');
+// main.js
+
+// Styles
+
+
+}();
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,4 +1,6 @@
 console.warn('Hello People!!!');
 // main.js
 
+// Styles
+import '../sass/main.scss';
 import './clock';
