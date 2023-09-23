a83da922b0b33aa400bbe025c5ed5393a4de0a58
Hekmat <hb8info@gmail.com>
2023-09-24 00:41:34 +0330

    Some changes in class-block-patterns

--- inc/classes/class-block-patterns.php
+++ inc/classes/class-block-patterns.php
@@ -30,32 +30,33 @@ class Block_Patterns {
 
 	public function register_block_patterns(): void {
 		if ( function_exists('register_block_pattern')){
+
+			/**
+			 * Cover pattern
+			 */
+			$cover_content = $this->get_pattern_content('template-parts/patterns/cover');
+
 			register_block_pattern(
 				'aquila/cover',
 				[
 					'title' => __('Aquila Cover', 'aquila'),
 					'description' => __('Aquila Cover Block with image and text', 'aquila'),
 					'categories'=> ['cover'],
-					'content' =>'
-					<!-- wp:cover {"url":"http://localhost/aquila/wp-content/uploads/2023/09/screenshot-1.png","id":1835,"dimRatio":50,"align":"full","layout":{"type":"constrained"}} -->
-						<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1835" alt="" src="http://localhost/aquila/wp-content/uploads/2023/09/screenshot-1.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
-						<h1 class="wp-block-heading alignwide has-text-align-center">Never let your memories greater than your dreams</h1>
-						<!-- /wp:heading -->
-						
-						<!-- wp:heading {"textAlign":"center","level":5,"textColor":"cyan-bluish-gray"} -->
-						<h5 class="wp-block-heading has-text-align-center has-cyan-bluish-gray-color has-text-color">A mind that is stretched by new experience can never go back to its old dimensions.</h5>
-						<!-- /wp:heading -->
-						
-						<!-- wp:buttons {"layout":{"type":"flex","verticalAlignment":"top","justifyContent":"center"}} -->
-						<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"cyan-bluish-gray","textColor":"white","style":{"border":{"radius":"25px"}},"className":"is-style-outline"} -->
-						<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-white-color has-cyan-bluish-gray-background-color has-text-color has-background has-text-align-center wp-element-button" style="border-radius:25px">Blogs</a></div>
-						<!-- /wp:button --></div>
-						<!-- /wp:buttons --></div></div>
-						<!-- /wp:cover -->
-					'
+					'content' => $cover_content
 				]
 			);
 		}
+	}
+	public function get_pattern_content($template_path){
+		ob_start();
+
+		get_template_part($template_path);
+
+		$pattern_content = ob_get_contents();
+
+		ob_end_clean();
+
+		return $pattern_content;
 	}
 
 	public function register_block_pattern_categories() {
---
+++ template-parts/patterns/cover.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Cover block pattern template
+ *
+ * @package aquila
+ */
+?>
+
+<!-- wp:cover {"url":"http://localhost/aquila/wp-content/uploads/2023/09/screenshot-1.png","id":1835,"dimRatio":50,"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1835" alt="" src="http://localhost/aquila/wp-content/uploads/2023/09/screenshot-1.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
+		<h1 class="wp-block-heading alignwide has-text-align-center">Never let your memories greater than your dreams</h1>
+		<!-- /wp:heading -->
+
+		<!-- wp:heading {"textAlign":"center","level":5,"textColor":"cyan-bluish-gray"} -->
+		<h5 class="wp-block-heading has-text-align-center has-cyan-bluish-gray-color has-text-color">A mind that is stretched by new experience can never go back to its old dimensions.</h5>
+		<!-- /wp:heading -->
+
+		<!-- wp:buttons {"layout":{"type":"flex","verticalAlignment":"top","justifyContent":"center"}} -->
+		<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"cyan-bluish-gray","textColor":"white","style":{"border":{"radius":"25px"}},"className":"is-style-outline"} -->
+			<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-white-color has-cyan-bluish-gray-background-color has-text-color has-background has-text-align-center wp-element-button" style="border-radius:25px">Blogs</a></div>
+			<!-- /wp:button --></div>
+		<!-- /wp:buttons --></div></div>
+<!-- /wp:cover -->
